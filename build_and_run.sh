deedf69808b67620a1f91b1df35aaa155def04b4
Kevin Chau <kkhaichau@gmail.com>
2022-05-15 12:44:00 -0700

    Propagate args

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker build . -t kkchau/trulia_to_notion:latest && docker run --rm -e NOTION_API_TOKEN=$NOTION_API_TOKEN kkchau/trulia_to_notion:latest
+docker build . -t kkchau/trulia_to_notion:latest && docker run --rm -e NOTION_API_TOKEN=$NOTION_API_TOKEN kkchau/trulia_to_notion:latest "$@"
