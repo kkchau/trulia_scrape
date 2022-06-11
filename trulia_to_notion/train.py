89df04158c4139c50fdcb88a7efc34f84b9b49ab
Kevin Chau <kkhaichau@gmail.com>
2022-06-11 15:23:23 -0700

    train on the whole dataset

--- trulia_to_notion/train.py
+++ trulia_to_notion/train.py
@@ -15,20 +15,20 @@ def train_classifier(input_data: pd.DataFrame):
     """Train classifier with input data"""
     x = input_data.loc[:, FEATURES]  # pylint: disable=C0103
     y = input_data.loc[:, SELECTOR_FIELD]  # pylint: disable=C0103
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.25, random_state=0
-    )
+    #x_train, x_test, y_train, y_test = train_test_split(
+    #    x, y, test_size=0.25, random_state=0
+    #)
 
     model = LogisticRegression()
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+    model.fit(x, y)
+    y_pred = model.predict(x)
 
     model_info = {
         "model": model,
-        "accuracy": metrics.accuracy_score(y_test, y_pred),
-        "precision": metrics.precision_score(y_test, y_pred),
-        "recall": metrics.recall_score(y_test, y_pred),
-        "confusion_matrix": str(metrics.confusion_matrix(y_test, y_pred)),
+        "accuracy": metrics.accuracy_score(y, y_pred),
+        "precision": metrics.precision_score(y, y_pred),
+        "recall": metrics.recall_score(y, y_pred),
+        "confusion_matrix": str(metrics.confusion_matrix(y, y_pred)),
     }
     logger.info(model_info)
     return model_info
